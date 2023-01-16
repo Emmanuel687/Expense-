cfda498421136d4f4b488d48fbc4ae0c05e97812
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-16 16:10:08 +0300

    Added Footer HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM  from "react-dom";
+// import React from "react";
+// import ReactDOM  from "react-dom";
 
-ReactDOM.render(
-    <h1>My App</h1>, document.getElementById('root')
-)
+// ReactDOM.render(
+//     <h1>My App</h1>, document.getElementById('root')
+// )
