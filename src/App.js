384d0073d437cc575f17eb47f8d539d8863d527b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-17 16:35:28 +0300

    React States

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 const App = () => {
   return (
     <div className="comments">
-      <Header/>
-     <h1>My App</h1>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
     </div>
   );
 };
