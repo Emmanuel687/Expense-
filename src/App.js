f1be435d5d136ad2acea017f5fbab66c9a85ce21
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-17 14:29:22 +0300

    Created the header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 const App = () => {
-  const name = "Emmanuel";
-  const body = "My name is Emmanul";
-  const comments = [
-    { id: 1, text: `Comment 1` },
-    { id: 2, text: `Comment 2` },
-    { id: 3, text: `Comment 3` },
-  ];
-
   return (
     <div className="comments">
-      <h1>{name.toUpperCase}</h1>
-      <p>{body}</p>
-      {Math.random() * 5}
-
-      <div>
-        <h3>Comments:{comments.length}</h3>
-        <ul>
-          {comments.map((comment,index)=>(
-            <l1 key={index}>{comment.text}</l1>
-          ))}
-        </ul>
-      </div>
+      <Header/>
+     <h1>My App</h1>
     </div>
   );
 };
