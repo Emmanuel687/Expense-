8873591e60c38cd2830eb6a3178ab2603228585b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-17 16:09:20 +0300

    Styling React Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React from "react"; 
+import PropTypes from 'prop-types';
 
-function Header() {
+function Header({text,bgColor,textColor}) {
+    const headerStyles ={
+        backgroundColor: bgColor,
+        color:textColor
+        
+    }
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
-        <h2>Header</h2>
+        <h2>{text}</h2>
       </div>
     </header>
   );
+}
+
+Header.defaultProps={
+    text:`Feedback Ui`,
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor:'#ff6a95'
+}
+
+Header.prototypes ={
+    text:PropTypes.string,
+    bgColor:PropTypes.string,
+    textColor:PropTypes.string
+
 }
 
 export default Header;
