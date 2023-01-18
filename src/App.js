140a808075585e257a1df23b0d19bb82db0f56c4
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-18 19:19:22 +0300

    Created Styled  Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./Data/FeedbackData";
 const App = () => {
-  const[feedback,setFeedback]=useState(FeedbackData)
+  const[feedback,setFeedback]=useState(FeedbackData);
+  const deleteFeedback = (id)=>{
+    if(window.confirm(`Are sure you want to Delete`))
+    setFeedback(feedback.filter(item=>item.id !==id))
+  }
   return (
     <div className="comments">
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </div>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback,handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item}></FeedbackItem>
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        ></FeedbackItem>
       ))}
     </div>
   );
 }
-FeedbackItem.PropTypes ={
-  feedback:PropTypes.arrayOf(
-    PropTypes.shape(
-      {
-        id:PropTypes.number.isRequired,
-        text:PropTypes.string.isRequired,
-        rating:PropTypes.number.isRequired,
-
-      }
-    )
-  )
-}
-
+FeedbackItem.PropTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
