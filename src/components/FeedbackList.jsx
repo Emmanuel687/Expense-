fd10f18b8df943ea2b3239c8c20e0dd5f71b4430
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-19 19:04:07 +0300

    Added Animation list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
-
-function FeedbackList({ feedback,handleDelete }) {
+import { motion, AnimatePresence, animate } from "framer-motion";
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        ></FeedbackItem>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{opacitty:0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            ></FeedbackItem>
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
