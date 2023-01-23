c0eb602c9b37b2aa1c8138c999a195aaf95df1f2
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-23 17:56:47 +0300

    Props updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,32 @@
-// import React from "react"; 
-// import PropTypes from 'prop-types';
+import React from "react"; 
+import PropTypes from 'prop-types';
 
-// function Header({text,bgColor,textColor}) {
-//     const headerStyles ={
-//         backgroundColor: bgColor,
-//         color:textColor
+function Header({text,bgColor,textColor}) {
+    const headerStyles ={
+        backgroundColor: bgColor,
+        color:textColor
         
-//     }
-//   return (
-//     <header style={headerStyles}>
-//       <div className="container">
-//         <h2>{text}</h2>
-//       </div>
-//     </header>
-//   );
-// }
+    }
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
 
-// Header.defaultProps={
-//     text:`Feedback UI`,
-//     bgColor:'rgba(0,0,0,0.4)',
-//     textColor:'#ff6a95'
-// }
+Header.defaultProps={
+    text:`Feedback UI`,
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor:'#ff6a95'
+}
 
-// Header.prototypes ={
-//     text:PropTypes.string,
-//     bgColor:PropTypes.string,
-//     textColor:PropTypes.string
+Header.prototypes ={
+    text:PropTypes.string,
+    bgColor:PropTypes.string,
+    textColor:PropTypes.string
 
-// }
+}
 
-// export default Header;
+export default Header;
