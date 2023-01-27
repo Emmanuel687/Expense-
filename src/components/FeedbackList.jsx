889af514998879f629b3f745e9170c5ce239ff8d
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-27 17:28:50 +0300

    Created Server.js

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,33 +1,46 @@
-import React from "react";
-import { useContext } from "react";
-import FeedbackItem from "./FeedbackItem";
-import { motion, AnimatePresence } from "framer-motion";
-import FeedbackContext from "../context/FeedbackContext";
-import Spinner from "./Shared/Spinner";
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
+import FeedbackContext from '../context/FeedbackContext'
+
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
 
 function FeedbackList() {
-  const { feedback, isLoading } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
   if (!isLoading && (!feedback || feedback.length === 0)) {
-    return <p>No Feedback yet</p>;
+    return <p>No Feedback Yet</p>
   }
-  return isLoading ? ( <Spinner/>
+
+  return isLoading ? (
+    <Spinner />
   ) : (
-    <div className="feedback-list">
+    <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => (
           <motion.div
             key={item.id}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            exit={{ opacitty: 0 }}
+            exit={{ opacity: 0 }}
+            layout
           >
-            <FeedbackItem key={item.id} item={item}></FeedbackItem>
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
     </div>
-  );
+  )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
-export default FeedbackList;
+export default FeedbackList
