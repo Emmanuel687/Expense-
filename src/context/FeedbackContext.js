472b87877c342a50130ef9e33d35360d5e69ee55
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-27 15:42:54 +0300

    Used the Post method in in add feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState,useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -29,9 +28,16 @@ export const FeedbackProvider = ({ children }) => {
       setFeedback(feedback.filter((item) => item.id !== id));
   };
   //Add Feedback Function
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+    const data = await response.json()
+    setFeedback([data, ...feedback]);
   };
 
   //Set item to be updated
